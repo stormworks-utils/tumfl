cee0efa01e28a4181f2f22c239f1defd6e149c6e
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-05-07 10:02:33 +0200

    actually use TumflException

--- tumfl/error.py
+++ tumfl/error.py
@@ -5,14 +5,14 @@ class TumflException(Exception):
     ...
 
 
-class ParserException(Exception):
+class ParserException(TumflException):
     def __init__(self, message: str, hints: list[tuple[Token, str, str]], token: Token):
         super().__init__(message)
         self.hints: list[tuple[Token, str, str]] = hints
         self.token: Token = token
 
 
-class LexerException(Exception):
+class LexerException(TumflException):
     def __init__(self, message: str, line: int, column: int):
         super().__init__(message)
         self.line: int = line
