19b6731c5e5ac33367a040d803a4e625aaf9be8b
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-05-22 17:00:56 +0200

    refactor chunk and nse version

--- tumfl/AST/Statement/Chunk.py
+++ tumfl/AST/Statement/Chunk.py
@@ -1,5 +1,20 @@
+from __future__ import annotations
+
+from tumfl.AST.Expression.Expression import Expression
+from tumfl.Token import Token
+
 from .Block import Block
+from .Statement import Statement
 
 
 class Chunk(Block):
-    """Same as block, but individually executable"""
+    """Same as block, but individually executable."""
+
+    def __init__(
+        self,
+        token: Token,
+        statements: list[Statement],
+        returns: list[Expression],
+    ):
+        super().__init__(token, statements, returns)
+        self.name = "Chunk"
