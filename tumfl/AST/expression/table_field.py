f181a2d002286276d4f2efc074f2bc8cf14ca680
Fabian Wunsch <fabian@uriah.heep.sax.de>
2025-05-17 15:50:06 +0200

    Move value to parent class

--- tumfl/AST/expression/table_field.py
+++ tumfl/AST/expression/table_field.py
@@ -13,28 +13,29 @@ if TYPE_CHECKING:
 class TableField(ASTNode, ABC):
     """Base class for all table fields"""
 
+    def __init__(self, token: Token, name: str, value: Expression):
+        super().__init__(token, name)
+        self.value: Expression = value
+
 
 class ExplicitTableField(TableField):
     """Table field of the form `[ expr ] = expr`"""
 
     def __init__(self, token: Token, at: Expression, value: Expression):
-        super().__init__(token, "ExplicitTableField")
+        super().__init__(token, "ExplicitTableField", value)
         self.at: Expression = at
-        self.value: Expression = value
 
 
 class NamedTableField(TableField):
     """Table field of the form `Name = expr`"""
 
     def __init__(self, token: Token, field_name: Name, value: Expression):
-        super().__init__(token, "NamedTableField")
+        super().__init__(token, "NamedTableField", value)
         self.field_name: Name = field_name
-        self.value: Expression = value
 
 
 class NumberedTableField(TableField):
     """Table field of the form `expr`"""
 
     def __init__(self, token: Token, value: Expression):
-        super().__init__(token, "NumberedTableField")
-        self.value: Expression = value
+        super().__init__(token, "NumberedTableField", value)
