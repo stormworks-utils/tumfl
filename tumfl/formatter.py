b6ff7c5d7fc62816fea30643c4b5a6f1901598c0
Fabian Wunsch <fabian@uriah.heep.sax.de>
2025-05-30 11:27:38 +0200

    Reformat

--- tumfl/formatter.py
+++ tumfl/formatter.py
@@ -123,11 +123,14 @@ class Formatter(BasicWalker[Retype]):
 
     def _format_comment(self, comment: str, force_long: bool = False) -> Retype:
         comment = comment.strip()
-        prefix: str = "-" if comment and comment[0] == '-' else ''
-        comment = comment.lstrip('-')
+        prefix: str = "-" if comment and comment[0] == "-" else ""
+        comment = comment.lstrip("-")
         if force_long or "\n" in comment:
             level: int = self._find_level(comment)
-            return [f"--{prefix}[{'=' * level}[{comment}]{'=' * level}]", Separators.Statement]
+            return [
+                f"--{prefix}[{'=' * level}[{comment}]{'=' * level}]",
+                Separators.Statement,
+            ]
         return [f"--{prefix}{self.s.COMMENT_SEP}{comment}", Separators.Newline]
 
     def _format_function_args(self, args: Sequence[Expression]) -> Retype:
